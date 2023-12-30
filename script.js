4113331878e4a099f2ce8365aab1b239e4680043
samyakmaitre <pixelpirate404@gmail.com>
2023-12-30 23:17:10 +0530

    fdjkg

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-
 $(window).on('load', function () {
     // Remove the loader once the page is fully loaded
     $('.loader-wrapper').fadeOut('slow');
 });
+
+function displayFileName(input) {
+    const label = document.querySelector('label[for="attachment"]');
+    const fileStatusLabel = document.getElementById('fileStatusLabel');
+
+    if (input.files.length > 0) {
+        label.innerHTML = `Attachment: ${input.files[0].name}`;
+        fileStatusLabel.style.display = 'none'; // Hide "No file chosen" text
+    } else {
+        label.innerHTML = 'Add an Attachment';
+        fileStatusLabel.style.display = 'inline'; // Show "No file chosen" text
+    }
+}
+
